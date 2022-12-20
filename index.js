f847ff039b17bb0ba11931824ebd64f8cfd70d56
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2022-12-20 14:39:38 +0100

    updating ingredients

--- index.js
+++ index.js
@@ -5,13 +5,26 @@ let guanciale = 250;
 let egg = 6;
 let cheese = 50;
 let pepper = 4;
+let water = 3000;
+let salt = 20;
 
-let BigBowl = egg + cheese;
+console.log("ingredients");
+console.log("spaghetti",spaghetti);
+console.log("guanciale",guanciale)
+console.log("egg",egg);
+console.log("cheese",cheese);
+console.log("pepper",pepper);
+console.log("water",water);
+console.log("salt",salt);
 
-//console.log(BigBowl);
+/*let cheeseSauce = egg + cheese;
+console.log(cheeseSauce)
 
-let pan = pepper 
-//console.log(pan)
+let dashOfPepper = pepper -1;
+console.log(dashOfPepper)
+let eggyCheese = cheeseSauce + dashOfPepper;
+console.log(eggyCheese)
+
 
 BigBowl = BigBowl + .25*pan;
 //console.log(BigBowl);
@@ -23,6 +36,6 @@ FryingPan = FryingPan + pot;
 FryingPan = FryingPan + BigBowl;
 FryingPan + .75 * pepper;
 
-console.log(FryingPan);
-
+//console.log(FryingPan);
+*/
 
