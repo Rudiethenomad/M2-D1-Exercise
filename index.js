7f53eed7673c16e7666176d4d48efa6bc811d76b
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2022-12-20 11:39:57 +0100

    adding structure
    ran first console log to make sure was working

---
+++ index.js
@@ -0,0 +1,11 @@
+// Exersice 1 recipe for carbonara
+
+let spaghetti = 400;
+let guanciale = 250;
+let egg = 6;
+let cheese = 50;
+let pepper = 4;
+
+let BigBowl = egg + cheese;
+
+console.log(BigBowl);
